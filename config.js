08979063eab6475f9975c139f58d84ace6dae879
Miras <220573@astanait.edu.kz>
2024-01-23 11:19:42 +0600

    first commit

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "36aa9bc2e32c7e658049dc64e9330c3b"
+    }
+}
+
+module.exports = constants;
